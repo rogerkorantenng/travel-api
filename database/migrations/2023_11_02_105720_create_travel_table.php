30b6c4a28a3cb7b08c266e834be198c2538dd85a
rogerkorantenng <rogerkorantenng@gmail.com>
2023-11-02 14:15:53 +0000

    build database tables

---
+++ database/migrations/2023_11_02_105720_create_travel_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    #create migratiions for id, is_public, slug, name, description, number_of_days, timestamps
+    public function up(): void
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->boolean(column:'is_public')->default(value:false);
+            $table->string(column:'slug')->unique();
+            $table->string(column:'name');
+            $table->text(column:'description');
+            $table->unsignedinteger(column:'number_of_days');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travel');
+    }
+};
